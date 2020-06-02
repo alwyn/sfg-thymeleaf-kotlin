80a08e51c58c392bb9202e4cf8055f997c193f07
Alwyn Schoeman <alwyn.schoeman@olspayments.com>
2020-06-02 11:31:27 -0400

    Initial commit

---
+++ src/main/kotlin/tech/nomads/learn/thymeleaf/SfgThymeleafCourseApplication.kt
@@ -0,0 +1,11 @@
+package tech.nomads.learn.thymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfgThymeleafCourseApplication
+
+fun main(args: Array<String>) {
+    runApplication<SfgThymeleafCourseApplication>(*args)
+}
---
+++ src/test/kotlin/tech/nomads/learn/thymeleaf/SfgThymeleafCourseApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.nomads.learn.thymeleaf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
